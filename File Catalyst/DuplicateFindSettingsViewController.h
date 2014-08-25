6054de87ed68d61dbeeff95b03a0c325ffdb25db
Viktoryia Labunets <vika@Viktoryia-Labunetss-MacBook.local>
2014-08-25 15:13:20 +0200

    First Commit

---
+++ File Catalyst/DuplicateFindSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DuplicateFindSettingsViewController.h
+//  File Catalyst
+//
+//  Created by Viktoryia Labunets on 25/08/14.
+//  Copyright (c) 2014 Nuno Brum. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DuplicateFindSettingsViewController : NSViewController
+
+@end
---
+++ File Catalyst/DuplicateFindSettingsViewController.m
@@ -0,0 +1,26 @@
+//
+//  DuplicateFindSettingsViewController.m
+//  File Catalyst
+//
+//  Created by Viktoryia Labunets on 25/08/14.
+//  Copyright (c) 2014 Nuno Brum. All rights reserved.
+//
+
+#import "DuplicateFindSettingsViewController.h"
+
+@interface DuplicateFindSettingsViewController ()
+
+@end
+
+@implementation DuplicateFindSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
