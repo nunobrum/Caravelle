4582b0b9d1e1a5d3dffed0e8887aee2662643bd5
Viktoryia Labunets <vika@Viktoryia-Labunetss-MacBook.local>
2014-09-02 15:37:23 +0200

    Adding AppOperations Moving TreeScanOperation to file Classes

---
+++ File Catalyst/FileClasses/AppOperation.h
@@ -0,0 +1,13 @@
+//
+//  AppOperation.h
+//  File Catalyst
+//
+//  Created by Viktoryia Labunets on 02/09/14.
+//  Copyright (c) 2014 Nuno Brum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppOperation : NSOperation
+
+@end
---
+++ File Catalyst/FileClasses/AppOperation.m
@@ -0,0 +1,13 @@
+//
+//  AppOperation.m
+//  File Catalyst
+//
+//  Created by Viktoryia Labunets on 02/09/14.
+//  Copyright (c) 2014 Nuno Brum. All rights reserved.
+//
+
+#import "AppOperation.h"
+
+@implementation AppOperation
+
+@end
